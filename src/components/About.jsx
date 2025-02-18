ae31a8181909636da71d287717f97403604dd5cb
parmarshashank <parmarshashank6@gmail.com>
2025-02-18 12:18:04 +0530

    changed folder structure

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -116,7 +116,7 @@ const About = () => {
           >
             <div className="bg-[#D7C9AE]/20 rounded-2xl h-[600px] w-full flex items-center justify-center">
               <p className="text-[#A68763] text-lg">
-                <img src="public/assets/p3.webp" alt="p1" className='w-full h-full object-cover' />
+                <img src="/p3.webp" alt="p1" className='w-full h-full object-cover' />
               </p>
             </div>
           </motion.div>
